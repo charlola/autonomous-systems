1252ca498fec058ea5a037271801c171c5ba2d55
charlola <=>
2021-06-13 23:43:29 +0200

    requirements.txt expanded

--- src/mlflow_logging.py
+++ src/mlflow_logging.py
@@ -6,16 +6,12 @@ def log_artifact_example():
         f.write("Thank Charlotte later")
     mlflow.log_artifact("output.txt")
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
+
+mlflow.set_tracking_uri("http://159.65.120.229:5000")
 mlflow.set_experiment("test")
 
 params = {"p1": "A", "p2": "B"}
 metric = {"m1": 12, "m2": 34}
 
-
 mlflow.log_metrics(metric)
 mlflow.log_params(params)
-
-
-
-
